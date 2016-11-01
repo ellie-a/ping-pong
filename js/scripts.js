90447ef933a36d8472f338a1b5a7e45e3c712a66
ea <elliea915@gmail.com>
2016-11-01 14:26:28 -0700

    add style to the css

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
 var game = function(number) {
   var pingPong = [];
-
   for (i = 1; i <= number; i++) {
-
     if (i % 3 === 0 && i % 5 !== 0){
-      pingPong.push("Ping");
+      pingPong.push("Ping!");
     } else if (i % 5 === 0 && i & 3 !== 0) {
-      pingPong.push("Pong");
+      pingPong.push("Pong!");
     } else if (i % 15 === 0 && i !== 0) {
-      pingPong.push("Ping-Pong");
+      pingPong.push("Ping-Pong!");
     } else {
       pingPong.push(i);
     }
@@ -17,19 +15,13 @@ var game = function(number) {
 };
 
 $(document).ready(function() {
-
   $("form#value").submit(function(event) {
-
     event.preventDefault();
-
     $("#userOutput ul").empty();
-
     var userInput = parseInt($("#userInput").val());
     var results = game(userInput);
-
     results.forEach(function(result) {
       $("#userOutput ul").append("<li>" + result + "</li>")
     });
-
   });
 });
